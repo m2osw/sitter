139314169e5733b0133b36d98c324e1bf67e5928
AlexisWilke <alexis@m2osw.com>
2022-06-05 16:09:41 -0700

    Updated the copyright notice to 2022.

--- sitter/meminfo.cpp
+++ sitter/meminfo.cpp
@@ -1,5 +1,5 @@
 // Snap Websites Server -- read /proc/meminfo
-// Copyright (c) 2018-2019  Made to Order Software Corp.  All Rights Reserved
+// Copyright (c) 2018-2022  Made to Order Software Corp.  All Rights Reserved
 //
 // https://snapwebsites.org/
 // contact@m2osw.com
